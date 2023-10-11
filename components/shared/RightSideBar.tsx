f61ec6d6d4f7000d98d618d76c1404671c155789
MartinZakhaev <andikawahyusyaputra@gmail.com>
2023-10-11 15:22:22 +0700

    added top questions and tags functionality

--- components/shared/RightSideBar.tsx
+++ components/shared/RightSideBar.tsx
@@ -2,44 +2,12 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import RenderTag from "./RenderTag";
+import { getHotQuestions } from "@/lib/actions/question.action";
+import { getTopPopularTags } from "@/lib/actions/tag.actions";
 
-const hotQuestions = [
-  {
-    _id: "1",
-    title:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam doloribus sequi magnam. Maiores sit mollitia esse optio ut facilis sapiente a dolorem voluptates velit, quas eum fugiat eligendi fugit adipisci.",
-  },
-  {
-    _id: "2",
-    title:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam doloribus sequi magnam. Maiores sit mollitia esse optio ut facilis sapiente a dolorem voluptates velit, quas eum fugiat eligendi fugit adipisci.",
-  },
-  {
-    _id: "3",
-    title:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam doloribus sequi magnam. Maiores sit mollitia esse optio ut facilis sapiente a dolorem voluptates velit, quas eum fugiat eligendi fugit adipisci.",
-  },
-  {
-    _id: "4",
-    title:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam doloribus sequi magnam. Maiores sit mollitia esse optio ut facilis sapiente a dolorem voluptates velit, quas eum fugiat eligendi fugit adipisci.",
-  },
-  {
-    _id: "5",
-    title:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam doloribus sequi magnam. Maiores sit mollitia esse optio ut facilis sapiente a dolorem voluptates velit, quas eum fugiat eligendi fugit adipisci.",
-  },
-];
-
-const popularTags = [
-  { _id: "1", name: "javascript", totalQuestions: 5 },
-  { _id: "2", name: "typescript", totalQuestions: 5 },
-  { _id: "3", name: "react js", totalQuestions: 5 },
-  { _id: "4", name: "flutter", totalQuestions: 5 },
-  { _id: "5", name: "react js", totalQuestions: 5 },
-];
-
-const RightSideBar = () => {
+const RightSideBar = async () => {
+  const hotQuestions = await getHotQuestions();
+  const popularTags = await getTopPopularTags();
   return (
     <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen w-[355px] flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
       <div>
@@ -47,7 +15,7 @@ const RightSideBar = () => {
         <div className="mt-7 flex w-full flex-col gap-[30px]">
           {hotQuestions.map((question) => (
             <Link
-              href={`/questions/${question._id}`}
+              href={`/question/${question._id}`}
               key={question._id}
               className="flex cursor-pointer items-center justify-between gap-7"
             >
@@ -73,7 +41,7 @@ const RightSideBar = () => {
               key={tag._id}
               _id={tag._id}
               name={tag.name}
-              totalQuestions={tag.totalQuestions}
+              totalQuestions={tag.numberOfQuestions}
               showCount
             />
           ))}
